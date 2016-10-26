be0ef43c499ab5bcaa6e40d0d1921e62cec209a5
LUKEPC\Luke <allhailluke@roantree.com>
2016-10-26 21:10:39 +0100

    initial commit: includes initial MLR model

---
+++ initial_trial.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 26 16:46:58 2016
+
+@author: Luke
+"""
+
+import pandas as pd
+from sklearn import linear_model 
+import numpy as np
+import statsmodels.formula.api as smf
+
+train = pd.read_csv('C:/Users/Luke/QUBHACKATHON/trainingData.csv') #You'll need to make these your own paths
+test = pd.read_csv('C:/Users/Luke/QUBHACKATHON/testingData.csv')
+
+
+regressors = train[[train.columns.values[7], train.columns.values[8], train.columns.values[38], train.columns.values[37]]]
+
+response= train['response']
+est = smf.OLS(response, regressors)
+est = est.fit()
+print est.summary()
+print type(est.summary())
+
+regressors_new = test[[test.columns.values[7], test.columns.values[8], test.columns.values[38], test.columns.values[37]]]
+
+print ((est.predict(regressors)*2.0).round())*1.0/1.0
+print response.values
+
+real = np.array(response.values)
+predicted = np.array(((est.predict(regressors)*2.0).round())*1.0/1.0)
+
+print np.average(real-predicted)
+
